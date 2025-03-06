8448b464413b7d7bf62ece79b6543532ecc95e05
maryt <maryt@Mariana>
2025-03-06 10:13:34 -0500

    Subiendo helperImpresion

---
+++ src/Helpers/HelperImpresion.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Helpers;
+
+import Logica_Negocio.Persona;
+
+public class HelperImpresion {
+    
+    public static void ImprimirPersona(Persona ObjPersona)
+    {
+         System.out.println("El nombre de la persona es"+"\t"+ObjPersona.getNombre());
+        System.out.println("El apellido de la persona es"+"\t"+ObjPersona.getApellido());
+        System.out.println("El id de la persona es"+"\t"+ObjPersona.getId());
+        System.out.println("El nombre del producto es"+"\t"+ObjPersona.getObjProctucto().getNombreProducto());
+        System.out.println("El id del producto es"+"\t"+ObjPersona.getObjProctucto().getIdProducto());
+        System.out.println("La descripcion del producto es"+"\t"+ObjPersona.getObjProctucto().getDescripcion());
+        
+    }
+    
+}
