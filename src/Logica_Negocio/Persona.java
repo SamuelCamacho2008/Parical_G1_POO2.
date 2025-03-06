a728120612ee02c0bad589f4537b44d80e506942
luisk <luisk@192.168.243.128>
2025-03-06 09:15:29 -0500

    Subiendo clase Persona

--- src/Logica_Negocio/Persona.java
+++ src/Logica_Negocio/Persona.java
@@ -6,8 +6,51 @@ package Logica_Negocio;
 
 /**
  *
- * @author camacho
+ * @author luiskandor
  */
 public class Persona {
+    String id, nombre, apellido;
+    public Producto objProctucto;
     
+    public Persona() {
+    }
+
+    public Persona(String id, String nombre, String apellido, Producto objProctucto) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.objProctucto = objProctucto;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Producto getObjProctucto() {
+        return objProctucto;
+    }
+
+    public void setObjProctucto(Producto objProctucto) {
+        this.objProctucto = objProctucto;
+    }
 }
