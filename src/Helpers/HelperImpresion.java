1ce260fc1b28851098ae75fe2b826162b7fdda9d
luisk <luisk@192.168.243.128>
2025-03-06 12:09:20 -0500

    corrigiendo errores

--- src/Helpers/HelperImpresion.java
+++ src/Helpers/HelperImpresion.java
@@ -10,12 +10,12 @@ public class HelperImpresion {
     
     public static void ImprimirPersona(Persona ObjPersona)
     {
-         System.out.println("El nombre de la persona es"+"\t"+ObjPersona.getNombre());
-        System.out.println("El apellido de la persona es"+"\t"+ObjPersona.getApellido());
-        System.out.println("El id de la persona es"+"\t"+ObjPersona.getId());
-        System.out.println("El nombre del producto es"+"\t"+ObjPersona.getObjProducto().getNombreProducto());
-        System.out.println("El id del producto es"+"\t"+ObjPersona.getObjProducto().getIdProducto());
-        System.out.println("La descripcion del producto es"+"\t"+ObjPersona.getObjProducto().getDescripcion());
+         System.out.println("El nombre de la persona es:"+"\t"+ObjPersona.getNombre());
+        System.out.println("El apellido de la persona es:"+"\t"+ObjPersona.getApellido());
+        System.out.println("El id de la persona es:"+"\t"+ObjPersona.getId());
+        System.out.println("El nombre del producto es:"+"\t"+ObjPersona.getObjProducto().getNombreProducto());
+        System.out.println("El id del producto es:"+"\t"+ObjPersona.getObjProducto().getIdProducto());
+        System.out.println("La descripcion del producto es:"+"\t"+ObjPersona.getObjProducto().getDescripcion());
         
     }
     
